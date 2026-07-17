f5893491e90679ae00600502ec7a4194448c282e
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-17 14:30:09 +0530

    Add files via upload

---
+++ 1-index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("sudhanshu ranjan");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is now running at port ${port}`);
+});
